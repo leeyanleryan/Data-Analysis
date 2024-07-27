d4f95e55ddfdfc0925a84e70123d0fc2fcb80360
leeyanleryan <leeyanleryan.21@gmail.com>
2024-07-27 17:07:35 +0800

    added folder

---
+++ Breast Cancer Analysis/DecisionTreeV2.r
@@ -0,0 +1,54 @@
+library(rpart)
+library(rpart.plot)
+
+cancer_data <- read.csv("CancerDataV2.csv")
+cancer_data[, 1:15] <- lapply(cancer_data[, 1:15], scale)
+n <- dim(cancer_data)[1]
+
+# 10 Fold Cross Validation
+n_folds <- 10
+folds_j <- sample(rep(1:n_folds, length.out = n))
+
+# 10 different values for Complexity Parameter
+cp <- 10^(-5:5)
+
+# 10 different values of accuracy for each CP
+accuracy <- rep(1, length(cp))
+
+for (i in 1:10) {
+correct <- 0
+for (j in 1:n_folds) {
+test <- which(folds_j == j)
+test_x <- cancer_data[test, 1:15]
+test_y <- cancer_data[test, 16]
+train <- cancer_data[-test, ]
+
+dt_model <- rpart(overall_survival ~ .,
+                  method = "class", data = train,
+                  control = rpart.control(cp = cp[i], minsplit = 1),
+                  parms = list(split = "information"))
+dt_pred <- predict(dt_model, newdata = test_x, type = "class")
+correct <- correct + sum(dt_pred == test_y)
+}
+accuracy[i] <- correct / n
+}
+
+# Plot graph of complexity parameter against accuracy
+plot(log(cp, base = 10), accuracy, type = "b")
+
+# Using best complexity parameter
+best_cp <- cp[which(accuracy == max(accuracy))]
+dt_model <- rpart(overall_survival ~ .,
+                  method = "class", data = cancer_data,
+                  control = rpart.control(cp = best_cp, minsplit = 1),
+                  parms = list(split = "information"))
+rpart.plot(dt_model, type = 4, extra = 2, clip.right.labs = FALSE,
+           varlen = 0, faclen = 0)
+
+# Normal plotting
+dt_model <- rpart(overall_survival ~ .,
+                  method = "class", data = cancer_data,
+                  control = rpart.control(minsplit = 1),
+                  parms = list(split = "information"))
+rpart.plot(dt_model, type = 4, extra = 2, clip.right.labs = FALSE,
+           varlen = 0, faclen = 0)
